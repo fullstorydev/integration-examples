a406a9ce497f377688d07448e6d54b22a45d2613
Van Staub <52075243+van-fs@users.noreply.github.com>
2021-08-11 12:24:12 -0400

    Session stitching example (#3)
    * initial design
    
    * improve tests and waitUntil
    
    * updated design and tests
    
    * call timeoutFn and end

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -7,8 +7,11 @@ Object.defineProperty(window, '_fs_namespace', {
 const FS = () => { };
 FS.event = jest.fn();
 FS.identify = jest.fn();
+FS.log = jest.fn();
+FS.restart = jest.fn();
 FS.setVars = jest.fn();
 FS.setUserVars = jest.fn();
+FS.shutdown = jest.fn();
 
 // NOTE this will always use the exemplar `FS` namespace
 Object.defineProperty(window, 'FS', {

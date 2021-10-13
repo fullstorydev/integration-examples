17af6f51f85903033489416a1c6e832442ef7153
van-fs <van@fullstory.com>
2021-10-13 09:21:39 -0400

    babel transpiled to supported browsers

--- dist/sampling.js
+++ dist/sampling.js
@@ -2,26 +2,26 @@
   'use strict';
 
   function sample(rate, daysValid) {
-    const cookieName = '_fs_sample_user';
+    var cookieName = '_fs_sample_user';
     try {
-      if (document.cookie.indexOf(`${cookieName}=true`) > -1 || document.cookie.indexOf(`${cookieName}=false`) > -1) {
-        return document.cookie.indexOf(`${cookieName}=true`) > -1;
+      if (document.cookie.indexOf("".concat(cookieName, "=true")) > -1 || document.cookie.indexOf("".concat(cookieName, "=false")) > -1) {
+        return document.cookie.indexOf("".concat(cookieName, "=true")) > -1;
       } else {
-        const shouldSample = (Math.random() < (rate / 100));
-        const days = (daysValid !== undefined && daysValid > 0) ? daysValid : 30;
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        document.cookie = `${cookieName}=${shouldSample}; expires=${date.toGMTString()}; path=/`;
+        var shouldSample = Math.random() < rate / 100;
+        var days = daysValid !== undefined && daysValid > 0 ? daysValid : 30;
+        var date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        document.cookie = "".concat(cookieName, "=").concat(shouldSample, "; expires=").concat(date.toGMTString(), "; path=/");
         return shouldSample;
       }
     } catch (err) {
-      console.error(`FullStory unavailable, unable to sample user`);
+      console.error("FullStory unavailable, unable to sample user");
       return false;
     }
   }
 
-  const rate = 10;
-  const daysValid = 90;
+  var rate = 10;
+  var daysValid = 90;
   if (sample(rate, daysValid)) {
     console.log('REPLACE THIS LINE WITH YOUR FULLSTORY SNIPPET');
   }

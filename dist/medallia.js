f654b4e86d946d600718adc11261e67bedaf3406
van-fs <van@fullstory.com>
2021-10-19 10:43:49 -0400

    add _fs_medallia_use_label feature

--- dist/medallia.js
+++ dist/medallia.js
@@ -4,13 +4,13 @@
   function fs(api) {
     if (!hasFs()) {
       return function () {
-        console.error(`FullStory unavailable, check your snippet or tag`);
-      }
+        console.error('FullStory unavailable, check your snippet or tag');
+      };
     } else {
       if (api && !window[window._fs_namespace][api]) {
         return function () {
-          console.error(`${window._fs_namespace}.${api} unavailable, update your snippet or verify the API call`);
-        }
+          console.error(window._fs_namespace + '.' + api + ' unavailable, update your snippet or verify the API call');
+        };
       }
       return api ? window[window._fs_namespace][api] : window[window._fs_namespace];
     }
@@ -20,25 +20,35 @@
   }
 
   function handleSubmitFeedback(event) {
-    const detail = event.detail;
+    var detail = event.detail;
     if (!detail) {
       fs('log')('warn', 'MDigital_Submit_Feedback data not found');
       return;
     }
-    const payload = {
+    var payload = {
       Form_Type: detail.Form_Type,
       Form_ID: detail.Form_ID,
-      Feedback_UUID: detail.Feedback_UUID,
+      Feedback_UUID: detail.Feedback_UUID
     };
-    for (let i = 0; i < detail.Content.length; i += 1) {
+    for (var i = 0; i < detail.Content.length; i += 1) {
       if (detail.Content[i].unique_name === 'NPS') {
-        fs('setUserVars')({ NPS: detail.Content[i].value });
+        fs('setUserVars')({
+          NPS: detail.Content[i].value
+        });
       } else {
-        payload[detail.Content[i].unique_name] = detail.Content[i].value;
+        if (window['_fs_medallia_use_label'] && detail.Content[i].label) {
+          payload[detail.Content[i].label.trim().replace(/\s/g, '_')] = detail.Content[i].value;
+        } else {
+          payload[detail.Content[i].unique_name] = detail.Content[i].value;
+        }
       }
     }
     fs('event')('Medallia Feedback', payload);
   }
-  window.addEventListener('MDigital_Submit_Feedback', handleSubmitFeedback);
+  window['_fs_medallia_use_label'] = false;
+  if (!window['_fs_medallia_feedback_registered']) {
+    window.addEventListener('MDigital_Submit_Feedback', handleSubmitFeedback);
+    window['_fs_medallia_feedback_registered'] = true;
+  }
 
 }());

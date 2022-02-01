ed1abb4e92b2f3c424f8b5a60551fcd2beb2a4b9
Irv Salisbury <irv@fullstory.com>
2022-02-01 09:24:35 -0500

    Fixing unit tests

--- __tests__/rate-limit-spec.js
+++ __tests__/rate-limit-spec.js
@@ -9,7 +9,7 @@ describe('Rate Limit Tests', () => {
     iterateFunction.mockClear();
   });
 
-  test('test rate-limit  10 calls', (done) => {
+  test('test rate-limit  10 per second calls', (done) => {
 
     let targetRatePerSecond = 10;
     let numSeconds = 3;
@@ -18,7 +18,7 @@ describe('Rate Limit Tests', () => {
 
   });
 
-  test('test rate-limit  100 second calls', (done) => {
+  test('test rate-limit  100 per second calls', (done) => {
 
     let targetRatePerSecond = 100;
     let numSeconds = 3;
@@ -30,10 +30,17 @@ describe('Rate Limit Tests', () => {
   function testIterateWithRate(targetRatePerSecond, numSeconds, iterationTimeEstimate, callbackFn) {
     iterateWithRate(targetRatePerSecond, numSeconds, iterationTimeEstimate,
       iterateFunction, (actualRate, totalTime) => {
-        // make sure all four functions get called
+      // setup a 10% buffer on either side for testing
+        let rateLow = targetRatePerSecond * .90;
+        let rateHigh = targetRatePerSecond * 1.10;
+        let totalTimeLow = numSeconds * .90;
+        let totalTimeHigh = numSeconds * 1.10;
+        let realTotalTime = totalTime / 1000;
+        // make sure it was called exactly the number of times expected
         expect(iterateFunction).toBeCalledTimes(targetRatePerSecond * numSeconds);
-        expect(Math.round(actualRate)).toEqual(targetRatePerSecond);
-        expect(Math.round(totalTime / 1000)).toEqual(numSeconds);
+        // make sure it is within rates.
+        expect((actualRate >= rateLow) && (actualRate <= rateHigh)).toBeTruthy();
+        expect((realTotalTime >= totalTimeLow) && (realTotalTime <= totalTimeHigh)).toBeTruthy();
         callbackFn();
       });
   }

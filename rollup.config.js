ef87a5bc19b3726fa9107ee37abd8ceac9e40a13
van-fs <van@fullstory.com>
2022-07-05 14:08:41 -0400

    build both minified and normal dist files

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
 import babel from '@rollup/plugin-babel';
 import cleanup from 'rollup-plugin-cleanup';
+import { terser } from 'rollup-plugin-terser';
 import * as fs from 'fs';
 
 const samples = fs.readdirSync(`${__dirname}/src`).filter(filename => filename.endsWith('.js'));
-const options = samples.map(filename => {
-  return {
+const options = [];
+
+samples.forEach(filename => {
+  // non-minified JS
+  options.push({
     input: `src/${filename}`,
     output: {
       file: __dirname + `/dist/${filename}`,
@@ -15,8 +19,26 @@ const options = samples.map(filename => {
         exclude: 'node_modules/**',
         babelHelpers: 'bundled'
       }),
-      cleanup()],
-  };
+      cleanup(),
+    ],
+  });
+
+  // minified JS
+  options.push({
+    input: `src/${filename}`,
+    output: {
+      file: __dirname + `/dist/${filename.replace('.js', '.min.js')}`,
+      format: 'iife'
+    },
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled'
+      }),
+      terser(),
+      cleanup(),
+    ],
+  });
 });
 
 export default options;
